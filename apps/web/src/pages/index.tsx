309d23e5790a4ca2f07ea820b71452a0cef9d9d3
Alexey Sokolov <95688435+alexvyber@users.noreply.github.com>
2022-10-16 14:55:14 +0300

    katzen image added

--- apps/web/src/pages/index.tsx
+++ apps/web/src/pages/index.tsx
@@ -1,5 +1,9 @@
 import Head from "next/head";
 import { Button } from "ui";
+import katzen from "../images/katzen.png"
+import Image from "next/future/image"
+
+const techs = ["Next", "Strapi", "TailwindCSS", "GraphQl"]
 
 export default function Home() {
   return (
@@ -10,13 +14,17 @@ export default function Home() {
       </Head>
 
       <main className="mx-auto w-auto px-4 pt-16 pb-8 sm:pt-24 lg:px-8">
-        <h1 className="mx-auto max-w-5xl sm:text-center text-6xl font-extrabold tracking-tight text-white">
+      <Image src={katzen} width={250} height={250} alt="katzen" className="mx-auto" />
+        <h1 className="mx-auto max-w-5xl text-center text-6xl font-extrabold tracking-tight text-white">
+        
           <span className="inline-block bg-gradient-to-r from-brandmain to-brandaccent bg-clip-text text-transparent  text-7xl sm:text-9xl">
             Katzen
           </span>
         </h1>
-        <h2 className="text-3xl mt-8 text-white">
-          Next + Strapi + TailwindCSS + GraphQL starter
+        <h2 className="text-3xl mt-8 text-white text-center ">
+          {
+            techs.map(item => <span key={item} className="block sm:inline">{item}{" "}</span>)
+          }
         </h2>
         <div className="mx-auto mt-8 max-w-xl sm:flex sm:justify-center md:mt-8">
           <Button />
