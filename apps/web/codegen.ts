cdfaba875575fdeab3cf1e0d950d1c0944513044
Alexey Sokolov <alexvyber@gmail.com>
2022-10-29 00:12:00 +0300

    configs

--- apps/web/codegen.ts
+++ apps/web/codegen.ts
@@ -2,7 +2,7 @@ import type { CodegenConfig } from "@graphql-codegen/cli";
 
 const config: CodegenConfig = {
   schema: "http://127.0.0.1:1337/graphql",
-  documents: ["src/**/*.tsx"],
+  documents: ["src/**/*.ts*"],
   generates: {
     "./src/gql/": {
       preset: "client",
