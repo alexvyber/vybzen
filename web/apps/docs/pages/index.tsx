5f869c96ba0295522cfb79dbedf8d1012a57e713
Alexey Sokolov <alexvyber@gmail.com>
2022-11-25 20:20:13 +0300

    pnpm reworked

---
+++ web/apps/docs/pages/index.tsx
@@ -0,0 +1,12 @@
+import { Button } from "ui";
+
+export default function Docs() {
+  return (
+    <div>
+      <h1>Docs</h1>
+      <Button href="https://github.com/alexvyber/katzen" intent="secondary">
+        GitHub
+      </Button>
+    </div>
+  );
+}
